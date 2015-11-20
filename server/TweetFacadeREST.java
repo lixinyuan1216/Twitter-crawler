8338021c6b2636dd8c9a563b2f5e3196ab0196af
lixinyuan1216 <lixinyuan1216@gmail.com>
2015-11-21 01:23:37 +1100

    Create TweetFacadeREST.java

---
+++ server/TweetFacadeREST.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tweetentity.service;
+
+import com.tweetentity.Tweet;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author lxy
+ */
+@Stateless
+@Path("com.tweetentity.tweet")
+public class TweetFacadeREST extends AbstractFacade<Tweet> {
+
+    @PersistenceContext(unitName = "TweetServerPU")
+    private EntityManager em;
+
+    public TweetFacadeREST() {
+        super(Tweet.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Tweet entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") String id, Tweet entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Tweet find(@PathParam("id") String id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Path("location/{location}")
+    @Produces({"application/xml", "application/json"})
+    public List<Tweet> findByCreatedLocation(@PathParam("location") String location) {
+        List<Tweet> tweets = em.createNamedQuery("Tweet.findByCreatedLocation").setParameter("createdLocation", location).getResultList();
+        return tweets;
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Tweet> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Tweet> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+}
